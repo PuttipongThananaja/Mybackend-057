892bc67550bf00cd7332453c4b748f73131faef7
PuttipongThananaja <kwanthailand034@gmail.com>
2024-07-25 13:47:10 +0700

    Mybackend057

---
+++ src/app/api/route.js
@@ -0,0 +1,65 @@
+export async function GET() {
+    try { 
+
+        return new Response(JSON.stringify({ message: "GET DATA OK"}), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+          });
+
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          });
+    }
+}
+
+
+export async function POST() {
+    try { 
+
+        return new Response(JSON.stringify({ message: "GET DATA OK"}), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+          });
+
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          });
+    }
+}
+
+
+export async function PUT() {
+    try { 
+
+        return new Response(JSON.stringify({ message: "GET DATA OK"}), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+          });
+
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          });
+    }
+}
+
+export async function DELETE() {
+    try { 
+
+        return new Response(JSON.stringify({ message: "GET DATA OK"}), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+          });
+
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          });
+    }
+}
