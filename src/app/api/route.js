52d7dbdd5b6edf2433136c2083983612df0c9c94
PuttipongThananaja <kwanthailand034@gmail.com>
2024-07-25 14:53:16 +0700

    initial commit

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,19 +1,33 @@
+// app/api/route.js
+import { NextResponse } from 'next/server';
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+client.connect();
+
+
+
 export async function GET() {
-    try { 
-
-        return new Response(JSON.stringify({ message: "GET DATA OK"}), {
-            status: 200,
-            headers: { "Content-Type": "application/json" },
+    try {
+          const result = await client.query('SELECT * FROM tbl_users');
+          return new Response(JSON.stringify(result.rows), {
+              status: 200,
+              headers: { "Content-Type": "application/json" },
           });
-
     } catch (error) {
-        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-            status: 500,
-            headers: { "Content-Type": "application/json" },
+      
+          return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+              status: 500,
+              headers: { "Content-Type": "application/json" },
           });
     }
-}
-
+  }
 
 export async function POST() {
     try { 
